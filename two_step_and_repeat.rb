4e0e8831efbcce9a6ffbe81352e94930b7709b9b
mimigood <mbgss07@gmail.com>
2018-08-07 21:34:18 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -1,17 +1,14 @@
 def first_steps
-  lop do 
+  loop do
     puts "Right foot back"
-  sleep(0.5)
+    sleep(0.5)
     puts "Left foot back"
-  sleep(0.5)
+    sleep(0.5)
     puts "Right foot back"
-  sleep(0.5)
+    sleep(0.5)
     puts "Stop"
-  sleep(1)
+    sleep(1)
   end 
-end 
-  # Write a loop that outputs "Right foot back", "Left foot back",
-  # "Right foot back", and "Stop", with sleep after each
 end
 
 def a_few_more_steps
@@ -24,21 +21,51 @@ puts "Right foot back"
 sleep(0.5)
 puts "Stop"
 sleep(1)
-puts "right foot steps right and back"
-sleep(0.5) 
-puts"left foot crosses over right" 
+puts "Right foot steps right and back"
 sleep(0.5)
-puts "right foot steps right"
+puts "Left foot crosses over right"
 sleep(0.5)
-puts "turn"
+puts "Right foot steps right"
+sleep(0.5)
+puts "Turn"
 sleep(1)
+end
+end
+
+def how_many_steps?
+  steps = 0
+loop do 
+  steps += 1 
+  puts steps 
+if steps %2 == 0 
+  puts 'Left'
+else
+  puts 'Right'
+end
+sleep(0.5)
+end
+end 
+
+def break_dance
+  steps = 0
+loop do 
+  steps += 1 
+  puts steps 
+if steps %2 == 0 
+  puts 'Left'
+else
+  puts 'Right'
+end
+sleep(0.5)
+if steps == 6
+break 
+end
 end 
 end 
 
-def how_many_steps?
-  steps = 0 
-  loop do 
-    
+break_dance
+
+
   # Write a loop that outputs the first two sets of steps in the Two-Step
 end
 
